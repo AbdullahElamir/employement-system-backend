08c41804c8a7afe245544e41b10d6eaf63930449
Abdullah Elamir <abdullah.el_ameer@yahoo.com>
2024-07-28 12:30:19 +0200

    adding two values to the weather

--- WebServerAPI/Controllers/WeatherForecastController.cs
+++ WebServerAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebServerAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Cloudy","Hot","Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
