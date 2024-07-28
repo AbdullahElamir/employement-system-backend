b86778b885e016a376f7e959400e1b8ed10045fe
Abdullah Elamir <abdullah.el_ameer@yahoo.com>
2024-07-28 12:35:33 +0200

    working on login api

--- WebServerAPI/obj/Debug/net8.0/WebServerAPI.AssemblyInfo.cs
+++ WebServerAPI/obj/Debug/net8.0/WebServerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebServerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08c41804c8a7afe245544e41b10d6eaf63930449")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebServerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebServerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
