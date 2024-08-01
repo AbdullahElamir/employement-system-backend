cc87c0ad26452fa3b6623498bc08e6e05c61d177
malek jarallah <malek.jarallah05@gmail.com>
2024-08-01 11:00:57 +0200

    hi

---
+++ WebServerAPI/Controllers/JobController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebServerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JobController : Controller
+    {
+        public String get()
+        {
+            return "Job Controller Test";
+        }
+    }
+}
